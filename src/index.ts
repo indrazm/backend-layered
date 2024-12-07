4ec724050e1fb276b309197593a3a8f90375df77
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-12-07 12:51:14 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Elysia } from "elysia";
 import { authRouter } from "./presentation/router/authRouter";
 import { noteRouter } from "./presentation/router/noteRouter";
+import swagger from "@elysiajs/swagger";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia({ prefix: "/api" })
-	.use(authRouter)
-	.use(noteRouter)
+const app = new Elysia()
+	.use(cors())
+	.use(
+		swagger({
+			path: "/docs",
+		}),
+	)
+	.group("/api", (app) => app.use(authRouter).use(noteRouter))
 	.listen(8000);
